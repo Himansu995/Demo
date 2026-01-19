1d684a496be2bfb9380fb2501ccca5c2fc55a77e
himansu <himansusheoran2006@gamil.com>
2026-01-19 21:31:55 +0530

    1st commit

---
+++ Git/demo.java
@@ -0,0 +1,25 @@
+//package Git;
+
+// Working dir.
+// Staging area- git will monitor these
+// if we add commit to file and make any changes then we have to move it tyo satging area again
+
+
+
+
+// git init (will be on branch master)
+// git status (to check)
+// to change branch delete git folder from ur working folder manually
+// git init -b main
+// git add filename.xyz or foldername to add to cache in git repo
+// git rm --cached filename to unstage
+// git log to see all commits
+// git commit -m "message"
+
+// commiting generates some random code [main (root-commit) 2f7061f]
+//      this is checksum value of 40 characters
+public class demo {
+    public static void main(String []args){
+
+    }
+}
